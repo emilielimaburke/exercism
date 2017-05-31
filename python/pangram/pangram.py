3501911659b3b0bbaa07193cc0004744953b1901
emilielimaburke <emilie.burke@gmail.com>
2017-05-30 21:19:58 -0500

    finish pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,12 @@
-def is_pangram():
-    pass
+import re
+
+def is_pangram(pangram):
+	"""Determine if a sentence is a pangram."""
+	pangram = re.sub(r'[\W]', '', pangram)
+	pangram = re.sub(r'[_0-9]', '', pangram)
+	output = set(pangram.lower())
+	return len(output) == 26
+
+print(is_pangram('the quick brown fox jumped over the lazy FOX'))
+
+#pangram = re.sub(r'[a-zA-Z]', '', pangram)
