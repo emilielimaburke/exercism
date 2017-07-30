555b129b58b884beed8b6cebca928f7840777ef6
emilielimaburke <emilie.burke@gmail.com>
2017-07-30 18:45:29 -0400

    started bob

---
+++ python/bob/bob.py
@@ -0,0 +1,11 @@
+def hey(phrase):
+	if phrase[-1] == "?":
+		return('Sure.')
+	elif phrase == phrase.upper():
+		return('Whoa, chill out!')
+	# elif 
+	# 	return('Fine. Be that way!')
+	else:
+		return('Whatever.')
+
+##question mark followed by white space
